46f29f6353a42f6460611f5e0d898359126b610a
davicho27 <davidmisaelromero27032015@gmail.com>
2022-09-21 11:23:54 -0500

    Con disenio

--- forms/form_file.py
+++ forms/form_file.py
@@ -16,7 +16,8 @@ class App:
         filenameTwoN = ""
         global filenameThreeN
         filenameThreeN = ""
-        
+
+
         def browseFiles():
             filename = filedialog.askopenfilename(initialdir = "/", 
                                                 title = "Select a File", 
@@ -83,7 +84,7 @@ class App:
         self.ventana.geometry('800x500')
         self.ventana.config(bg='#05B7D0')
         self.ventana.resizable(width=0, height=0)
-        utl.centrar_ventana(self.ventana, 800, 520)
+        utl.centrar_ventana(self.ventana, 800, 560)
 
         label_title= Label(self.ventana,  
                             text = "Búsqueda .TXT",font=("Times New Roman", 25, 'bold'),
@@ -101,7 +102,13 @@ class App:
        
         button_explore = Button(self.ventana,  
                                 text = "Buscar", 
-                                command = browseFiles)
+                                command = browseFiles,
+                                bg='#007D8F',  
+                                fg = 'white', 
+                                height = 2, 
+                                width = 14)
+        
+
 
         label_file_explorer_two = Label(self.ventana,  
                     text = "Archivo .txt de la base de datos 'línea por línea'", 
@@ -112,11 +119,15 @@ class App:
 
         button_explore_two = Button(self.ventana,  
                         text = "Buscar", 
-                        command = browseFilesTwo)
+                        command = browseFilesTwo,
+                        bg='#007D8F',  
+                        fg = 'white', 
+                        height = 2, 
+                        width = 14)
 
         label_file_explorer_three = Label(self.ventana,  
                     text = "Destino del archivo", 
-                    width = 90, height = 6,
+                    width = 90, height = 4,
                     font=("Arial", 11),
                     bg= '#ffffff',   
                     fg = "black") 
@@ -125,27 +136,35 @@ class App:
         button_process = Button(self.ventana,  
                     text = "Guardar datos", 
                     command = buttonProcess,
-                    state = DISABLED)  
+                    state = DISABLED,
+                    bg='#25FF7E',  
+                    fg = 'white', 
+                    height = 2, 
+                    width = 14)  
         
         button_exit = Button(self.ventana,  
                             text = "Salir", 
-                            command = exit)  
+                            command = exit,
+                            bg='#F8413C',  
+                            fg = 'white', 
+                            height = 2, 
+                            width = 14)  
         
         label_title.grid(column = 1, row = 1) 
 
         label_file_explorer.grid(column = 1, row = 2) 
         
-        button_explore.grid(column = 1, row = 3) 
+        button_explore.grid(column = 1, row = 3, pady = 4, padx = 100) 
 
         label_file_explorer_two.grid(column = 1, row = 4) 
         
-        button_explore_two.grid(column = 1, row = 5) 
+        button_explore_two.grid(column = 1, row = 5, pady = 4, padx = 100)
         
         label_file_explorer_three.grid(column = 1, row = 6) 
 
-        button_process.grid(column = 1, row = 9 )
+        button_process.grid(column = 1, row = 9, pady = 8, padx = 100)
         
-        button_exit.grid(column = 1, row = 11) 
+        button_exit.grid(column = 1, row = 11, pady = 8, padx = 100) 
 
         self.ventana.mainloop()
     
